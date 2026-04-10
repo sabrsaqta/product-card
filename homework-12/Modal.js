528ce6d5004b5bba66dc4bd09704cfe27df7b2e6
sabrsaqta <1sabrsaqta1@gmail.com>
2026-04-11 01:05:41 +0500

    fixed

--- homework-12/Modal.js
+++ homework-12/Modal.js
@@ -2,35 +2,34 @@ export class Modal {
   constructor(modalId, buttonId, shouldCloseOnOverlay) {
     this.modal = document.getElementById(modalId);
     this.overlay = document.getElementById('overlay');
+    this.closeButton = this.modal.querySelector('#modal-close-button');
     this.callCloseProperty = this.callClose.bind(this); //назвал callCloseProperty, чтобы не путаться с методом, для понимания своего
     this.shouldCloseOnOverlay = shouldCloseOnOverlay;
     this.#initOpen(buttonId);
-    this.#initClose(shouldCloseOnOverlay);
   }
 
   open() {
+    this.#initClose(this.shouldCloseOnOverlay); //вешаем слушатель на крестик
     this.modal.classList.add('modal-showed');
     this.overlay.classList.add('overlay-showed');
     if(this.shouldCloseOnOverlay){
-      this.overlay.addEventListener('click', this.callCloseProperty);
+      this.overlay.addEventListener('click', this.callClose);
     }
   }
-
+  
   close() {
     this.modal.classList.remove('modal-showed');
     this.overlay.classList.remove('overlay-showed');
-    this.overlay.removeEventListener('click', this.callCloseProperty);
+    this.overlay.removeEventListener('click', this.callClose);
+    this.closeButton.removeEventListener('click', this.callClose); //снимаем слушатель с крестика
+  }
+
+  callClose = () => {
+    this.close();
   }
 
   isOpen() {
     return this.modal.classList.contains('modal-showed');
-  }
-
-  callClose(){
-    this.close();
-    if(!this.isOpen()) {
-      this.overlay.removeEventListener('click', this.callCloseProperty);
-    }
   }
 
   #initOpen(buttonId) {
@@ -42,8 +41,6 @@ export class Modal {
 
   #initClose(shouldCloseOnOverlay) {
     const closeButton = this.modal.querySelector('#modal-close-button')
-    closeButton.addEventListener('click', () => {
-      this.close();
-    })
+    closeButton.addEventListener('click', this.callClose);
   }
 }
